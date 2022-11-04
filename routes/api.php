236b481c55a79bc316d94a554fe22b29f7c6e069
samantha-spindola <gisellesamspindola@gmail.com>
2022-11-04 11:18:29 -0600

    Nuevos controladores de todas las areas y rutas modificadas

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Ventas\VentasController;
 use App\Http\Controllers\Control\ControlController;
 use App\Http\Controllers\LM\LMController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Chofer\ChoferController;
+use App\Http\Controllers\Maquilador\MaquiladorController;
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,33 +31,32 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 Route::get('users', [AuthController::class, 'allUsers']);
 
 // RUTAS DE VENTAS
-
 // pagina principal
 Route::get('dashboard', [VentasController::class, 'dashboard']);
-
 // incidencias
 Route::get('incidencias', [VentasController::class, 'incidencias']);
 Route::get('incidencias/{pedido}', [VentasController::class, 'showIncidencia']);
-
 Route::post('aprobarInc', [VentasController::class, 'aprobarInc']);
 Route::post('rechazarInc', [VentasController::class, 'rechazarInc']);
-
 // seguimiento de pedido
 Route::get('pedidos', [VentasController::class, 'pedidos']);
-
 // pedido
 Route::get('pedidos/{pedido}', [VentasController::class, 'pedido']);
 
 // RUTAS DE CALIDAD_CONTROL
 Route::get('dashboardCon', [ControlController::class, 'dashboardCon']);
-
 //segpedidos
 Route::get('segpedidos', [ControlController::class, 'segpedidos']);
 //almacen
 Route::get('almacen', [ControlController::class, 'almacen']);
 Route::get('almacen/{pedido}', [ControlController::class, 'almacenShow']);
-// pedido
 Route::get('pedidosAL/{pedido}', [ControlController::class, 'pedidoAl']);
+Route::get('FormatodeInspeccion', [ControlController::class, 'FormatodeInspeccion']);
+Route::post('FirmaElaboroAceptar', [ControlController::class, 'FirmaElaboroAceptar']);
+Route::post('FirmaElaboroLimpiar', [ControlController::class, 'FirmaElaboroLimpiar']);
+Route::post('FirmaRevisoAceptar', [ControlController::class, 'FirmaRevisoAceptar']);
+Route::post('FirmaRevisoLimpiar', [ControlController::class, 'FirmaRevisoLimpiar']);
+Route::post('GuardarIns', [ControlController::class, 'GuardarIns']);
 
 // RUTAS DE lOGISTICA Y MESA DE CONTROL (LM)
 Route::get('dashboardLM', [LMController::class, 'dashboardLM']);
@@ -69,3 +69,23 @@ Route::get('rutasmat/{pedido}', [LMController::class, 'pedidomaterial']);
 Route::get('pedidos', [LMController::class, 'pedidos']);
 Route::get('pedidosLM/{pedido}', [LMController::class, 'pedidoLM']);
 
+// RUTAS DE CHOFER
+//Home chofer
+Route::get('dashboardChofer', [ChoferController::class, 'dashboardChofer']);
+//rutas de entrega pagina
+Route::get('RutasdeEntrega', [ChoferController::class, 'RutasdeEntrega']);
+Route::get('RutasdeEntrega/{pedido}', [ChoferController::class, 'PedidoEntrega']);
+//botones
+Route::get('Confirmar', [ChoferController::class, 'Confirmar']);
+Route::get('Rechazar', [ChoferController::class, 'Rechazar']);
+//Rutas de material limpio
+Route::get('RutasdeMaterial', [ChoferController::class, 'RutasdeMaterial']);
+Route::get('RutasdeEntrega', [ChoferController::class, 'RutasdeEntrega']);
+Route::get('RutasdeMaterial/{pedido}', [ChoferController::class, 'PedidoMaterial']);
+//Rutas de pedidos
+Route::get('SeguimientoPedidos', [ChoferController::class, 'Seguimientopedidos']);
+//RUTAS DEL MAQUILADOR
+//Home de maquilador
+Route::get('dashboardMaq', [MaquiladorController::class, 'dashboardMaq']);
+Route::get('Remisiones', [MaquiladorController::class, 'Remisiones']);
+Route::get('Remisiones/{pedido}', [MaquiladorController::class, 'DetallesR']);
