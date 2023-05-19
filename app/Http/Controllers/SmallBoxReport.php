f682b835a620891fc06ef3ea2df7cda5b46b7e5e
Oscar-CR <krz.oficial@gmail.com>
2023-05-19 10:35:08 -0600

    avance de formato de excel en espera a validacion de documento generado en la vista

--- app/Http/Controllers/SmallBoxReport.php
+++ app/Http/Controllers/SmallBoxReport.php
@@ -9,9 +9,9 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class SmallBoxReport extends Controller
 {
-    public function smallBoxReport()
+    public function smallBoxReport($purchases, $filter_data)
     {
-        //Personal de alta
+      
         $spreadsheet = new Spreadsheet();
 
         $sheet = $spreadsheet->getActiveSheet();
@@ -89,75 +89,22 @@ class SmallBoxReport extends Controller
                 'size' => 12
             ]
         ];
-        $sheet->getStyle('A1:G51')->applyFromArray($styleBorders);
-        $sheet->getStyle('A53:G56')->applyFromArray($styleBorders);
-        $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(34);
-        $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(36);
-        $spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(15);
-        $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(4);
-        $spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(8);
-        $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(6);
-        $spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(4);
-        $spreadsheet->getActiveSheet()->getRowDimension('52')->setRowHeight(5);
-        $sheet->getStyle('A2')->applyFromArray($styleTitle);
-        $sheet->getStyle('B4:G4')->applyFromArray($styleRows);
-        $sheet->getStyle('B6:G6')->applyFromArray($styleRows);
-        $sheet->getStyle('B8:G8')->applyFromArray($styleRows);
-        $sheet->getStyle('B10:G10')->applyFromArray($styleRows);
-        $sheet->getStyle('B12:G12')->applyFromArray($styleRows);
-        $sheet->getStyle('B14:G14')->applyFromArray($styleRows);
-        $sheet->getStyle('B16')->applyFromArray($styleRows);
-        $sheet->getStyle('F16:G16')->applyFromArray($styleRows);
-        $sheet->getStyle('B19:G19')->applyFromArray($styleRows);
-        $sheet->getStyle('B21:G21')->applyFromArray($styleRows);
-        $sheet->getStyle('B23:G23')->applyFromArray($styleRows);
-        $sheet->getStyle('B25:G25')->applyFromArray($styleRows);
-        $sheet->getStyle('B27:G27')->applyFromArray($styleRows);
-        $sheet->getStyle('B29:G29')->applyFromArray($styleRows);
-        $sheet->getStyle('B31:G31')->applyFromArray($styleRows);
-        $sheet->getStyle('B33:G33')->applyFromArray($styleRows);
-        $sheet->getStyle('B35:G35')->applyFromArray($styleRows);
-        $sheet->getStyle('B37:G37')->applyFromArray($styleRows);
+    
+        //Ajuste de espacio de las columnas
+        $spreadsheet->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('E')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('F')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('G')->setAutoSize(true);
+     
+        //Asignacion de estilos a las celdas
+        $sheet->getStyle('A1:G1')->applyFromArray($styleBorders);
+        $sheet->getStyle('A2:G2')->applyFromArray($styleBorders);
 
-        $sheet->getStyle('B39:D39')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('E39')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('F39')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('G39:F39')->applyFromArray($styleBeneficiary);
-
-        $sheet->getStyle('B40:D40')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('E40')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('F40')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('G40:F40')->applyFromArray($styleBeneficiary);
-
-        $sheet->getStyle('B41:D41')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('E41')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('F41')->applyFromArray($styleBeneficiary);
-        $sheet->getStyle('G41:F41')->applyFromArray($styleBeneficiary);
-
-        $sheet->getStyle('B43:G43')->applyFromArray($styleRows);
-        $sheet->getStyle('B45:G45')->applyFromArray($styleRows);
-        $sheet->getStyle('B47:G47')->applyFromArray($styleRows);
-        $sheet->getStyle('B48:G48')->applyFromArray($styleRows);
-        $sheet->getStyle('B49:G49')->applyFromArray($styleRows);
-        $sheet->getStyle('B50:G50')->applyFromArray($styleRows);
-        $sheet->getStyle('A53:G53')->applyFromArray($styleExclusive);
-
-        $sheet->getStyle('E54:G54')->applyFromArray($styleExclusiveBody);
-        $sheet->getStyle('E55:G55')->applyFromArray($styleExclusiveBody);
-        $sheet->getStyle('C54:D55')->applyFromArray($styleExclusiveBody);
-        $sheet->getStyle('A56')->applyFromArray($styleExclusiveBody);
-        $sheet->getStyle('B54:B55')->applyFromArray($styleExclusiveBody);
-        $sheet->getStyle('B56')->applyFromArray($styleExclusiveBody);
-        $sheet->getStyle('C56:G56')->applyFromArray($styleExclusiveBody);
-
-        $sheet->setCellValue('A2', 'REPORTE ');
-        $sheet->setCellValue('A4', 'EMPRESA:');
-        $sheet->setCellValue('C54', 'SD');
-        $sheet->setCellValue('C55', 'SBC');
-        $sheet->setCellValue('A56', 'OBSERVACIONES');
-        $sheet->setCellValue('B56', 'NUM DE EMPLEADO EN NOMINA');
-        $sheet->setCellValue('D56', 'SALARIO');
-        $sheet->setCellValue('B53','EXCLUSIVO RECURSOS HUMANOS');
+        $sheet->setCellValue('A1', 'REPORTE EMPRESA : '. strtoupper($filter_data[0]->company));
+        $sheet->setCellValue('A2', 'PERIODO : '. $filter_data[0]->start . ' AL ' . $filter_data[0]->end);;
 
         $writer = new Xlsx($spreadsheet);
         header('Content-Type: application/vnd.ms-excel');
--- app/Http/Controllers/SmallBoxUserController.php
+++ app/Http/Controllers/SmallBoxUserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\PaymentMethod;
 use App\Models\PurchaseRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -51,8 +53,39 @@ class SmallBoxUserController extends Controller
 
     public function report(Request $request)
     {
-       
+        $request->validate([
+            'start' => 'required',
+            'end' => 'required',
+            'payment_method_id' => 'required',
+            'company_id' => 'required',
+        ]);
+
+        $filter_data = [];
+
+        $format_start =date('Y-m-d', strtotime($request->start));
+        $format_end =date('Y-m-d', strtotime($request->end));
+
+        $payment_method = PaymentMethod::where('id', $request->payment_method_id)->get()->last();
+        $company = Company::where('id',$request->company_id)->get()->last();
+
+        if($payment_method  == null){
+            return response()->json(['msg' => "Metodo de pago no encontrado, verifica la información e intenta nuevamente"]);
+        }
+
+        if($company  == null){
+            return response()->json(['msg' => "Empresa no encontrada, verifica la información e intenta nuevamente"]);
+        }
+
+        array_push($filter_data, (object)[
+            'start' => $format_start, 
+            'end' => $format_start,
+            'payment_method' => $payment_method->name,
+            'company' => $company->name
+        ]);
+
+        $purchases = PurchaseRequest::whereDate('created_at','>=',$format_start)->whereDate('created_at','<=',$format_end)->where('company_id',$request->company_id)->where('payment_method_id',$request->payment_method_id)->get();
+
         $reporte = new SmallBoxReport();
-        $reporte->smallBoxReport();
+        $reporte->smallBoxReport($purchases, $filter_data);
     }
 }
