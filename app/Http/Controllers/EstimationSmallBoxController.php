b08061ba69feb37ec50bb34726e0809dbf77b29e
Carlos180200 <reyesmartinezcarlos2000@hotmail.com>
2024-01-30 18:54:02 -0600

    se agrego una restricción, si la devolución fue rechazada no se regresa el dinero

--- app/Http/Controllers/EstimationSmallBoxController.php
+++ app/Http/Controllers/EstimationSmallBoxController.php
@@ -51,9 +51,10 @@ class EstimationSmallBoxController extends Controller
                 $subquery->where('purchase_status_id', '=', 2)->where('type_status', '=', 'normal')->where('payment_method_id', '=', 1);
             })->orWhere(function ($subquery) {
                 $subquery->where('purchase_status_id', '=', 3)->where('type_status', '=', 'normal')->where('payment_method_id', '=', 1);
-            
             })->orWhere(function ($subquery){
                 $subquery->where('purchase_status_id', '=', 5)->where('type_status', '=', 'en proceso')->where('payment_method_id', '=', 1);
+            })->orWhere(function($subquery){
+                $subquery->where('purchase_status_id', '=', 5)->where('type_status', '=', 'rechazada')->where('payment_method_id', '=', 1);
             });
         })->sum('total');
 
