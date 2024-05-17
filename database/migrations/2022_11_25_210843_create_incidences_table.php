c0427592801ca8d6555264736f1c88890729d2b1
samantha-spindola <gisellesamspindola@gmail.com>
2024-05-17 15:07:14 -0600

    cambios en las incidencias

--- database/migrations/2022_11_25_210843_create_incidences_table.php
+++ database/migrations/2022_11_25_210843_create_incidences_table.php
@@ -33,6 +33,7 @@ class CreateIncidencesTable extends Migration
             $table->string('type_of_technique')->nullable();
             $table->string('user_solution')->nullable();
             $table->date('creation_date')->nullable();
+            $table->date('status');
             //Creacion de llave foranea
             $table->foreignId('sale_id')
                 ->nullable()
