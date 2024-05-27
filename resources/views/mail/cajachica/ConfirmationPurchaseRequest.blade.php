742d0a79928c339996cc1fe14fa7d915bf9a8972
carlos180200 <reyesmartinezcarlos2000@hotmail.com>
2024-05-27 13:59:11 -0600

    se modifico el texto

--- resources/views/mail/cajachica/ConfirmationPurchaseRequest.blade.php
+++ resources/views/mail/cajachica/ConfirmationPurchaseRequest.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
 # ¡Buen día, {{ $receptor}}!
 
-{{ $emisor }} ha confirmato la entrega de efectivo de la solicitud "GTO-{{$idPurchase}}".
-Apartir de ahora puedes pasar con el personal de caja chica para la entrega de dinero. 
+{{ $emisor }} ha confirmado la entrega de efectivo de la solicitud "GTO-{{$idPurchase}}".
+Apartir de ahora puedes pasar con el personal de Caja chica para la entrega de dinero. 
 
 Recuerda que una vez hayas recibido el dinero, deberás confirmar su recepción en el sistema. 
 De lo contrario, ya no se te volverá a entregar efectivo hasta que hablemos con tu jefe directo.
