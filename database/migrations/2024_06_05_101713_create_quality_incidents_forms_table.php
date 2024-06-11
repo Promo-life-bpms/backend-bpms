30caa173a73d2b0171d252a790c12d31efc6f17e
carlos180200 <reyesmartinezcarlos2000@hotmail.com>
2024-06-11 09:09:37 -0600

    se agrego el CRUD del formulario de incidencias

--- database/migrations/2024_06_05_101713_create_quality_incidents_forms_table.php
+++ database/migrations/2024_06_05_101713_create_quality_incidents_forms_table.php
@@ -15,23 +15,23 @@ class CreateQualityIncidentsFormsTable extends Migration
     {
         Schema::create('quality_incidents_forms', function (Blueprint $table) {
             $table->id();
-            $table->string('code_sale');
-            $table->string('incidence_folio');
-            $table->integer('days_in_warehouse');
-            $table->dateTime('incident_date');
+            $table->string('code_sale')->nullable();
+            $table->string('incidence_folio')->nullable();
+            $table->integer('days_in_warehouse')->nullable();
+            $table->dateTime('incident_date')->nullable();
             $table->foreignId('id_sale_product')->references('id')->on('sales_products')->onDelete('cascade');
-            $table->integer('sale_product_quantity');
-            $table->string('logo');
+            $table->integer('sale_product_quantity')->nullable();
+            $table->string('logo')->nullable();
             $table->foreignId('id_order_products')->references('id')->on('order_purchase_products')->onDelete('cascade');
-            $table->integer('order_product_quantity');
+            $table->integer('order_product_quantity')->nullable();
             $table->string('maquilador')->nullable();
             $table->string('distributor')->nullable();
-            $table->integer('correct_parts');
-            $table->integer('defective_parts');
-            $table->integer('defect_percentage');
-            $table->string('responsible');
-            $table->string('general_cause');
-            $table->string('return_description');
+            $table->integer('correct_parts')->nullable();
+            $table->integer('defective_parts')->nullable();
+            $table->integer('defect_percentage')->nullable();
+            $table->string('responsible')->nullable();
+            $table->string('general_cause')->nullable();
+            $table->string('return_description')->nullable();
             $table->timestamps();
         });
     }
